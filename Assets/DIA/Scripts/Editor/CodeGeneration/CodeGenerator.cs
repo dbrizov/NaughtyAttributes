76b295ad195d66019a98bf682e6148ffcd4a95e4
Denis Rizov <d.b.rizov@gmail.com>
2017-10-17 13:18:57 +0300

    Started implementing code generation

---
+++ Assets/DIA/Scripts/Editor/CodeGeneration/CodeGenerator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+public class CodeGenerator : Editor
+{
+    private const string GENERATED_CODE_TARGET_RELATIVE_PATH = "Assets/DIA/Scripts/Editor/Utility/";
+
+    private static readonly string ENTRIES_PLACEHOLDER = "__entries__";
+    private static readonly string VALIDATOR_ENTRY_FORMAT = "validatorsByAttributeType[typeof({0})] = new {1}();" + Environment.NewLine;
+
+    [UnityEditor.Callbacks.DidReloadScripts]
+    private static void GenerateCode()
+    {
+        string templateGUID = AssetDatabase.FindAssets("ValidatorUtilityTemplate")[0];
+        string templateRelativePath = AssetDatabase.GUIDToAssetPath(templateGUID);
+        string templateFullPath = (Application.dataPath.Replace("Assets", string.Empty) + templateRelativePath).Replace("/", "\\");
+
+        string templateFormat = IOUtility.ReadFromFile(templateFullPath);
+        Debug.Log(templateFormat);
+
+        StringBuilder validatorEntriesBuilder = new StringBuilder();
+        List<Type> validatorTypes = GetAllSubTypes(typeof(PropertyValidator));
+
+        foreach (var validatorType in validatorTypes)
+        {
+            PropertyValidatorAttribute[] attributes = (PropertyValidatorAttribute[])validatorType.GetCustomAttributes(typeof(PropertyValidatorAttribute), true);
+            if (attributes.Length > 0)
+            {
+                validatorEntriesBuilder.AppendFormat(VALIDATOR_ENTRY_FORMAT, attributes[0].TargetAttributeType.Name, validatorType.Name);
+            }
+        }
+
+        string validatorUtilityClassContent = templateFormat.Replace(ENTRIES_PLACEHOLDER, validatorEntriesBuilder.ToString());
+        Debug.Log(validatorUtilityClassContent); 
+    }
+
+    private static List<Type> GetAllSubTypes(Type baseClass)
+    {
+        var result = new List<Type>();
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        foreach (var assemly in assemblies)
+        {
+            Type[] types = assemly.GetTypes();
+            foreach (var type in types)
+            {
+                if (type.IsSubclassOf(baseClass))
+                {
+                    result.Add(type);
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ Assets/DIA/Scripts/Editor/Utility/IOUtility.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.IO;
+
+public static class IOUtility
+{
+    public static string GetPersistentDataPath()
+    {
+        return Application.persistentDataPath + "/";
+    }
+
+    public static void WriteToFile(string filePath, string content)
+    {
+        FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+        using (fileStream)
+        {
+            StreamWriter streamWriter = new StreamWriter(fileStream, System.Text.Encoding.ASCII);
+            using (streamWriter)
+            {
+                streamWriter.WriteLine(content);
+            }
+        }
+    }
+
+    public static string ReadFromFile(string filePath)
+    {
+        FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        using (fileStream)
+        {
+            StreamReader streamReader = new StreamReader(fileStream, System.Text.Encoding.ASCII);
+            using (streamReader)
+            {
+                string content = streamReader.ReadToEnd();
+                return content;
+            }
+        }
+    }
+
+    public static bool FileExists(string filePath)
+    {
+        return File.Exists(filePath);
+    }
+
+    public static string GetPathRelativeToProjectFolder(string fullPath)
+    {
+        int indexOfAssetsWord = fullPath.IndexOf("\\Assets");
+        string relativePath = fullPath.Substring(indexOfAssetsWord + 1);
+
+        return relativePath;
+    }
+}
