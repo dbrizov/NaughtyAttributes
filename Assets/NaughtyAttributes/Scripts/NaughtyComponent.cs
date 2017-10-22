fb93a073b5b46b9a47ef87523c8496965f2f7c46
Denis Rizov <d.b.rizov@gmail.com>
2017-10-22 13:54:11 +0300

    Added documentation pictures

--- Assets/NaughtyAttributes/Scripts/NaughtyComponent.cs
+++ Assets/NaughtyAttributes/Scripts/NaughtyComponent.cs
@@ -3,17 +3,6 @@ using UnityEngine;
 
 public class NaughtyComponent : MonoBehaviour
 {
-    public bool show;
-
     [MinMaxSlider(0f, 100f)]
-    [ValidateInput("ValidateSlider")]
     public Vector2 slider;
-
-    [Required]
-    public Transform trans;
-
-    private bool ValidateSlider(Vector2 slider)
-    {
-        return slider.magnitude > 0f;
-    }
 }
