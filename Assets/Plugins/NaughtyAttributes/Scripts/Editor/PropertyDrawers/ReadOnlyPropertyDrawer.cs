d5777007c256675861c6f90b621abc902f6427b8
nxrighthere <nxrighthere@gmail.com>
2017-10-24 12:11:01 +0000

    ReadOnly attribute implemented

---
+++ Assets/Plugins/NaughtyAttributes/Scripts/Editor/PropertyDrawers/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace NaughtyAttributes.Editor
+{
+    [PropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyPropertyDrawer : PropertyDrawer
+    {
+        public override void DrawProperty(SerializedProperty property)
+        {
+        	EditorGUI.BeginDisabledGroup(true);
+			EditorGUILayout.PropertyField(property, true);
+			EditorGUI.EndDisabledGroup();
+        }
+    }
+}
