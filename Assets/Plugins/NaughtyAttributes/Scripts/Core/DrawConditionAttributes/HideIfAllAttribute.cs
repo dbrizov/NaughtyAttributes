21d6c55c1b8173e850c21a802f9bb51ea433e810
Muitsonz <muiso8421@gmail.com>
2019-01-11 18:17:02 +0800

    add HideIfAny & HideIfAll

---
+++ Assets/Plugins/NaughtyAttributes/Scripts/Core/DrawConditionAttributes/HideIfAllAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NaughtyAttributes
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
+    public class HideIfAllAttribute : DrawConditionAttribute
+    {
+        public string ConditionName { get; private set; }
+
+        public bool ConditionValue { get; private set; }
+
+        public HideIfAllAttribute(string conditionName, bool conditionValue = true)
+        {
+            this.ConditionName = conditionName;
+            this.ConditionValue = conditionValue;
+        }
+    }
+}
---
+++ Assets/Plugins/NaughtyAttributes/Scripts/Editor/PropertyDrawConditions/HideIfAllPropertyDrawCondition.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+namespace NaughtyAttributes.Editor
+{
+    [PropertyDrawCondition(typeof(HideIfAllAttribute))]
+    public class HideIfAllPropertyDrawCondition : PropertyDrawCondition
+    {
+        public override bool CanDrawProperty(SerializedProperty property)
+        {
+            HideIfAllAttribute[] hideIfAllAttributes = PropertyUtility.GetAttributes<HideIfAllAttribute>(property);
+            UnityEngine.Object target = PropertyUtility.GetTargetObject(property);
+
+            foreach (var attribute in hideIfAllAttributes)
+            {
+                FieldInfo conditionField = ReflectionUtility.GetField(target, attribute.ConditionName);
+                if (conditionField != null &&
+                    conditionField.FieldType == typeof(bool))
+                {
+                    if ((bool)conditionField.GetValue(target) != attribute.ConditionValue)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                MethodInfo conditionMethod = ReflectionUtility.GetMethod(target, attribute.ConditionName);
+                if (conditionMethod != null &&
+                    conditionMethod.ReturnType == typeof(bool) &&
+                    conditionMethod.GetParameters().Length == 0)
+                {
+                    if ((bool)conditionMethod.Invoke(target, null) != attribute.ConditionValue)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                string warning = attribute.GetType().Name + " needs a valid boolean condition field or method name to work";
+                EditorDrawUtility.DrawHelpBox(warning, MessageType.Warning, logToConsole: true, context: target);
+            }
+
+            return false;
+        }
+    }
+}
