7f6c92d818fa505cce67f841068a6d8f1ff7230a
WCampos <wcampos.pro@gmail.com>
2019-07-15 15:49:41 -0700

    Completed Feature GetSetAttribute
    Trigger a Get and Set calls in a Property from a modifying a Field in the Inspector
    [GetSet("PropertyName")]
    private int _Field;
    public int PropertyName=> _Field;

---
+++ Assets/NaughtyAttributes/Scripts/Test/GetSetProperties.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using NaughtyAttributes;
+
+public class GetSetProperties : MonoBehaviour
+{
+    [SerializeField, GetSet("MyInt")]
+    private int _MyInt;
+    public int MyInt
+    {
+        get
+        {
+            print("Getting");
+            return _MyInt;
+        }
+        set
+        {
+            print("Setting");
+            _MyInt = value;
+        }
+    }
+
+
+    [Button]
+    private void IncrementProperty()
+    {
+        MyInt++;
+    }
+
+    [Button]
+    private void ReadProperty()
+    {
+        var i = MyInt;
+    }
+}
