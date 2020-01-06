1bfdde2041f9f4cbf7419c4e40f2e1f6f0a5a15a
Alex <45370547+SanielX@users.noreply.github.com>
2020-01-07 00:47:27 +0200

    Add files via upload

---
+++ Assets/NaughtyAttributes/Scripts/Core/AnimatorTypeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using NaughtyAttributes;
+using System;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+public class AnimatorTypeAttribute : DrawerAttribute
+{
+  //  public enum paramType { Float, Int, Bool, Trigger }
+
+    public AnimatorControllerParameterType Type;
+    public string FieldName;
+
+    public AnimatorTypeAttribute(string animatorFieldName, AnimatorControllerParameterType type)
+    {
+        Type = type;
+        FieldName = animatorFieldName;
+    }
+}
