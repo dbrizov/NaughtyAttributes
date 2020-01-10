1b350759837b5d30cf0ec136db0b17454b22db54
Aitor Iribar <aitor.iribar@gmail.com>
2020-01-10 16:47:33 +0100

    Added a test for UsePropertySetter.

---
+++ Assets/NaughtyAttributes/Scripts/Test/UsePropertySetter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace NaughtyAttributes.Test
+{
+    [ExecuteAlways]
+    public class UsePropertySetter : MonoBehaviour
+    {
+        public int MyPositiveProperty
+        {
+            get => _myPositiveProperty;
+            set
+            {
+                if (value >= 0)
+                {
+                    _myPositiveProperty = value;
+                    OnNewValue?.Invoke(value);
+                }
+                else Debug.Log("Negative! Not setting that.");
+            }
+        }
+        [SerializeField, UsePropertySetter, Tooltip("This is a working tooltip."), Delayed] private int _myPositiveProperty;
+
+        public event System.Action<int> OnNewValue;
+
+        private void OnEnable()
+        {
+            OnNewValue -= PrintValue;
+            OnNewValue += PrintValue;
+        }
+
+        private void PrintValue(int a)
+        {
+            print($"New value: {a}!");
+        }
+    }
+}
