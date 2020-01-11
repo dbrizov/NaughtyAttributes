31c7fc68dd78b74fb3c02eb8c29ff60f3e09fc22
dbrizov <d.b.rizov@gmail.com>
2020-01-11 23:00:29 +0200

    fix: MinMaxSlider attribute can now be nested

---
+++ Assets/NaughtyAttributes/Scripts/Editor/PropertyDrawers/NaughtyPropertyDrawer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace NaughtyAttributes.Editor
+{
+	public class NaughtyPropertyDrawer : PropertyDrawer
+	{
+		public UnityEngine.Object GetTargetObject(SerializedProperty property)
+		{
+			return property.serializedObject.targetObject;
+		}
+	}
+}
