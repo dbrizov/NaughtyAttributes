c6870e0aeffca993f5d701c30c2761b2bd440250
dbrizov <d.b.rizov@gmail.com>
2020-01-12 19:52:31 +0200

    added DropdownAttribute

--- Assets/NaughtyAttributes/Scripts/Test/Dropdowns.cs
+++ Assets/NaughtyAttributes/Scripts/Test/Dropdowns.cs
@@ -5,26 +5,30 @@ namespace NaughtyAttributes.Test
 {
 	public class Dropdowns : MonoBehaviour
 	{
-		//[Dropdown("intValues")]
+		[Dropdown("intValues")]
 		public int intValue;
 
-		//[Dropdown("stringValues")]
+		[Dropdown("StringValues")]
 		public string stringValue;
 
-		//[Dropdown("vectorValues")]
+		[Dropdown("GetVectorValues")]
 		public Vector3 vectorValue;
 
 #pragma warning disable 414
 		private int[] intValues = new int[] { 1, 2, 3 };
-
-		private List<string> stringValues = new List<string>() { "A", "B", "C" };
-
-		//private DropdownList<Vector3> vectorValues = new DropdownList<Vector3>()
-		//{
-		//	{ "Right", Vector3.right },
-		//	{ "Up", Vector3.up },
-		//	{ "Forward", Vector3.forward }
-		//};
 #pragma warning restore 414
+
+		private List<string> StringValues { get { return new List<string>() { "A", "B", "C" }; } }
+
+		private DropdownList<Vector3> GetVectorValues()
+		{
+			return new DropdownList<Vector3>()
+				{
+					{ "Right", Vector3.right },
+					{ "Up", Vector3.up },
+					{ "Forward", Vector3.forward },
+					{ "Back", Vector3.back }
+				};
+		}
 	}
 }
