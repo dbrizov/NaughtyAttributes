19b82a59feea478931564aad10c6971595cc528c
dbrizov <d.b.rizov@gmail.com>
2020-01-16 21:15:45 +0200

    add: ResizableTextAreaAttribute

---
+++ Scripts/Core/DrawerAttributes/ResizableTextAreaAttribute.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System;
+
+namespace NaughtyAttributes
+{
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+	public class ResizableTextAreaAttribute : PropertyAttribute, IDrawerAttribute
+	{
+	}
+}
--- Scripts/Editor/PropertyDrawers/IPropertyDrawer.cs
+++ Scripts/Editor/PropertyDrawers/IPropertyDrawer.cs
@@ -12,12 +12,12 @@ namespace NaughtyAttributes.Editor
 
 	public class PropertyDrawerBase : PropertyDrawer, IPropertyDrawer
 	{
-		public float GetPropertyHeight(SerializedProperty property)
+		public virtual float GetPropertyHeight(SerializedProperty property)
 		{
 			return EditorGUI.GetPropertyHeight(property, true);
 		}
 
-		public float GetHelpBoxHeight()
+		public virtual float GetHelpBoxHeight()
 		{
 			return EditorGUIUtility.singleLineHeight * 3.0f;
 		}
@@ -25,7 +25,7 @@ namespace NaughtyAttributes.Editor
 		public void DrawDefaultPropertyAndHelpBox(Rect rect, SerializedProperty property, string message, MessageType messageType)
 		{
 			Rect helpBoxRect = new Rect(
-					rect.x,
+					rect.x + NaughtyEditorGUI.GetIndentLength(rect),
 					rect.y,
 					rect.width,
 					GetHelpBoxHeight() - 2.0f);
