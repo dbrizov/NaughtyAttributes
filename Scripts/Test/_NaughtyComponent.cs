aa932a04b7f2032e3a0cebfa6936cef06d41ab57
dbrizov <d.b.rizov@gmail.com>
2020-01-20 20:28:27 +0200

    update: README.md

--- Scripts/Test/_NaughtyComponent.cs
+++ Scripts/Test/_NaughtyComponent.cs
@@ -5,36 +5,11 @@ namespace NaughtyAttributes.Test
 {
 	public class _NaughtyComponent : MonoBehaviour
 	{
-		[ReorderableList]
-		public int[] list;
+		[Label("Short Name")]
+		public string veryVeryLongName;
 
-		public MyClass myClass;
-
-#pragma warning disable 414
-		[ShowNonSerializedField]
-		private int myInt = 10;
-
-		[ShowNonSerializedField]
-		private const float PI = 3.14159f;
-
-		[ShowNonSerializedField]
-		private static readonly Vector3 CONST_VECTOR = Vector3.one;
-#pragma warning restore 414
-
-		[ShowNativeProperty]
-		private Transform Transform
-		{
-			get
-			{
-				return transform;
-			}
-		}
-
-		[Button]
-		private void LogParent()
-		{
-			Debug.Log(transform.parent);
-		}
+		[Label("RGB")]
+		public Vector3 vectorXYZ;
 	}
 
 	[System.Serializable]
