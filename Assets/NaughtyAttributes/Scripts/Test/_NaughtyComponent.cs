4b62264b36bbd2608ca5a981a0fbd2ecece35340
dbrizov <d.b.rizov@gmail.com>
2020-03-15 10:39:03 +0200

    OnValueChanged function signature no longer takes 2 parameters (oldValue, newValue). Now it takes 0 parameters, just like in NA v1.0.0 I decided to do this because for reference types the oldValue and newValue are pointers to the same object in memory so they are equal and it's a little confusing. This also fixes the null reference exception when OnValueChanged is used in nestes lists/arrays.

--- Assets/NaughtyAttributes/Scripts/Test/_NaughtyComponent.cs
+++ Assets/NaughtyAttributes/Scripts/Test/_NaughtyComponent.cs
@@ -5,39 +5,15 @@ namespace NaughtyAttributes.Test
 {
 	public class _NaughtyComponent : MonoBehaviour
 	{
-		public bool show1;
-		public bool show2;
-
-		[ShowIf(EConditionOperator.And, "show1", "show2")]
-		public int[] showIfAll;
-
-		[ShowIf(EConditionOperator.Or, "show1", "show2")]
-		public int[] showIfAny;
-
-		public MyClass nest1;
 	}
 
 	[System.Serializable]
 	public class MyClass
 	{
-		public bool show1;
-		public bool show2;
-		public bool Show1 { get { return show1; } }
-		public bool Show2 { get { return show2; } }
-
-		[ShowIf(EConditionOperator.And, "Show1", "Show2")]
-		[AllowNesting] // Because it's nested we need to explicitly allow nesting
-		public int[] showIfAll;
-
-		[ShowIf(EConditionOperator.Or, "Show1", "Show2")]
-		[AllowNesting] // Because it's nested we need to explicitly allow nesting
-		public int[] showIfAny;
 	}
 
-	//[System.Serializable]
-	//public struct MyStruct
-	//{
-	//	[ResizableTextArea]
-	//	public string level2;
-	//}
+	[System.Serializable]
+	public struct MyStruct
+	{
+	}
 }
