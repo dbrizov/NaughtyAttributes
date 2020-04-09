1ace233981e2c63a536692c2604b3e7bd8f3408c
Lucas Pereira Vasconcelos <lucas.pv@gilp.studio>
2020-04-09 13:50:29 -0300

    made OnEnable and OnDisable methods virtual and changed visibility of all draw methods to protected to improove extensibility of NaughtyInspector.

--- Scripts/Editor/NaughtyInspector.cs
+++ Scripts/Editor/NaughtyInspector.cs
@@ -15,7 +15,7 @@ namespace NaughtyAttributes.Editor
 		private IEnumerable<PropertyInfo> _nativeProperties;
 		private IEnumerable<MethodInfo> _methods;
 
-		private void OnEnable()
+		protected virtual void OnEnable()
 		{
 			_nonSerializedFields = ReflectionUtility.GetAllFields(
 				target, f => f.GetCustomAttributes(typeof(ShowNonSerializedFieldAttribute), true).Length > 0);
@@ -27,7 +27,7 @@ namespace NaughtyAttributes.Editor
 				target, m => m.GetCustomAttributes(typeof(ButtonAttribute), true).Length > 0);
 		}
 
-		private void OnDisable()
+		protected virtual void OnDisable()
 		{
 			ReorderableListPropertyDrawer.Instance.ClearCache();
 		}
@@ -67,7 +67,7 @@ namespace NaughtyAttributes.Editor
 			}
 		}
 
-		private void DrawSerializedProperties()
+		protected void DrawSerializedProperties()
 		{
 			serializedObject.Update();
 
@@ -107,7 +107,7 @@ namespace NaughtyAttributes.Editor
 			serializedObject.ApplyModifiedProperties();
 		}
 
-		private void DrawNonSerializedFields()
+		protected void DrawNonSerializedFields()
 		{
 			if (_nonSerializedFields.Any())
 			{
@@ -123,7 +123,7 @@ namespace NaughtyAttributes.Editor
 			}
 		}
 
-		private void DrawNativeProperties()
+		protected void DrawNativeProperties()
 		{
 			if (_nativeProperties.Any())
 			{
@@ -139,7 +139,7 @@ namespace NaughtyAttributes.Editor
 			}
 		}
 
-		private void DrawButtons()
+		protected void DrawButtons()
 		{
 			if (_methods.Any(m => ButtonUtility.IsVisible(serializedObject.targetObject, m)))
 			{
