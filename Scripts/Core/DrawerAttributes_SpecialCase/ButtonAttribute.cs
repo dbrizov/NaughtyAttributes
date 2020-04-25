5c71a943570c28ca746bf3384e152121ed6bfd23
dbrizov <d.b.rizov@gmail.com>
2020-04-25 19:01:10 +0300

    Some code formatting

--- Scripts/Core/DrawerAttributes_SpecialCase/ButtonAttribute.cs
+++ Scripts/Core/DrawerAttributes_SpecialCase/ButtonAttribute.cs
@@ -5,29 +5,29 @@ namespace NaughtyAttributes
 	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 	public class ButtonAttribute : SpecialCaseDrawerAttribute
 	{
-        public string Text { get; private set; }
-        public EnableMode SelectedEnableMode { get; private set; }
+		public string Text { get; private set; }
+		public EnableMode SelectedEnableMode { get; private set; }
 
-        public enum EnableMode
-        {
-            /// <summary>
-            /// Button should be active always
-            /// </summary>
-            Always,
-            /// <summary>
-            /// Button should be active only in editor
-            /// </summary>
-            Editor,
-            /// <summary>
-            /// Button should be active only in playmode
-            /// </summary>
-            Playmode
-        }
+		public enum EnableMode
+		{
+			/// <summary>
+			/// Button should be active always
+			/// </summary>
+			Always,
+			/// <summary>
+			/// Button should be active only in editor
+			/// </summary>
+			Editor,
+			/// <summary>
+			/// Button should be active only in playmode
+			/// </summary>
+			Playmode
+		}
 
-        public ButtonAttribute(string text = null, EnableMode enabledMode = EnableMode.Always)
-        {
-            this.Text = text;
-            this.SelectedEnableMode = enabledMode;
-        }
-    }
+		public ButtonAttribute(string text = null, EnableMode enabledMode = EnableMode.Always)
+		{
+			this.Text = text;
+			this.SelectedEnableMode = enabledMode;
+		}
+	}
 }
