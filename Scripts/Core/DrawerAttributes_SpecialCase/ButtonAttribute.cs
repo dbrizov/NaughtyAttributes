6f18c349be90026787f40f639ebcadcb090f8e28
dbrizov <d.b.rizov@gmail.com>
2020-04-26 12:36:57 +0300

    Renamed the ButtonAttribute.EnableMode enum to just EButtonEnableMode for consistency with the other enums in the project

--- Scripts/Core/DrawerAttributes_SpecialCase/ButtonAttribute.cs
+++ Scripts/Core/DrawerAttributes_SpecialCase/ButtonAttribute.cs
@@ -2,29 +2,29 @@
 
 namespace NaughtyAttributes
 {
+	public enum EButtonEnableMode
+	{
+		/// <summary>
+		/// Button should be active always
+		/// </summary>
+		Always,
+		/// <summary>
+		/// Button should be active only in editor
+		/// </summary>
+		Editor,
+		/// <summary>
+		/// Button should be active only in playmode
+		/// </summary>
+		Playmode
+	}
+
 	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 	public class ButtonAttribute : SpecialCaseDrawerAttribute
 	{
 		public string Text { get; private set; }
-		public EnableMode SelectedEnableMode { get; private set; }
+		public EButtonEnableMode SelectedEnableMode { get; private set; }		
 
-		public enum EnableMode
-		{
-			/// <summary>
-			/// Button should be active always
-			/// </summary>
-			Always,
-			/// <summary>
-			/// Button should be active only in editor
-			/// </summary>
-			Editor,
-			/// <summary>
-			/// Button should be active only in playmode
-			/// </summary>
-			Playmode
-		}
-
-		public ButtonAttribute(string text = null, EnableMode enabledMode = EnableMode.Always)
+		public ButtonAttribute(string text = null, EButtonEnableMode enabledMode = EButtonEnableMode.Always)
 		{
 			this.Text = text;
 			this.SelectedEnableMode = enabledMode;
