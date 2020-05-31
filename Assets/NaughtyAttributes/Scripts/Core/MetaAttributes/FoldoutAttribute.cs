0f91b4f427252fdf91b5b2a9f61513d3f58d215e
Benjamin Lumbye <benjamin@lumbye.net>
2020-05-31 14:20:27 +0200

    Implemented foldout attribute

---
+++ Assets/NaughtyAttributes/Scripts/Core/MetaAttributes/FoldoutAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NaughtyAttributes
+{
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+	public class FoldoutAttribute : MetaAttribute
+	{
+		public string Name { get; private set; }
+
+		public FoldoutAttribute(string name = "")
+		{
+			Name = name;
+		}
+	}
+}
