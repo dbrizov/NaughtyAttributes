2429ef8178f433e814bfd86e849345f0f07e2f15
Shane Celis <shane.celis@gmail.com>
2020-06-23 05:51:37 -0400

    refactor: Use attribute properties in constructor.

--- Assets/NaughtyAttributes/Scripts/Test/HeaderImageTest.cs
+++ Assets/NaughtyAttributes/Scripts/Test/HeaderImageTest.cs
@@ -5,19 +5,45 @@ namespace NaughtyAttributes.Test
 
 public class HeaderImageTest : MonoBehaviour
 {
-  public const string ICON
+  public const string ICON0
     = "Assets/NaughtyAttributes/Samples/DemoScene/TestAssets/icon-github.png";
-  [HeaderImage(ICON)]
+  public const string ICON
+    = "Assets/NaughtyAttributes/Samples/DemoScene/TestAssets/quadratic.png";
+  [HeaderImage(ICON, EAlignment.Center)]
   public Sprite sprite0;
 
   [HeaderImage(ICON, 96, 96)]
-  public GameObject prefab0;
+  public float a;
+
+  [HeaderImage(ICON, Height = 96)]
+  public float a1;
+
+  [HeaderImage(ICON0, Width = 96)]
+  public float a0;
 
   [HeaderImage(ICON, 96, 96, EAlignment.Center)]
-  public GameObject prefab1;
+  public float b;
+
+  [HeaderImage(ICON, Width = 96, Alignment = EAlignment.Center)]
+  public float b1;
+
+  [HeaderImage(ICON, Height = 96, Alignment = EAlignment.Center)]
+  public float b2;
+
+  [HeaderImage(ICON0, 96, 96, EAlignment.Center)]
+  public float b0;
 
   [HeaderImage(ICON, 96, 96, EAlignment.Right)]
-  public GameObject prefab2;
+  public float c;
+
+  [HeaderImage(ICON, Width = 96, Alignment = EAlignment.Right)]
+  public float c1;
+
+  [HeaderImage(ICON, Height = 96, Alignment = EAlignment.Right)]
+  public float c2;
+
+  [HeaderImage(ICON0, 96, 96, EAlignment.Right)]
+  public float c0;
 
   public HeaderImageNest1 nest1;
 }
