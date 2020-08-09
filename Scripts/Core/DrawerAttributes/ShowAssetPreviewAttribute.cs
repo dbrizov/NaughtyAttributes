6f8a858253ff8e1b8004a78910f6984611f47637
dbrizov <d.b.rizov@gmail.com>
2020-08-09 13:20:03 +0300

    Fixed #123 - [ReorderableList] and [ShowAssetPreview] can now be combined

--- Scripts/Core/DrawerAttributes/ShowAssetPreviewAttribute.cs
+++ Scripts/Core/DrawerAttributes/ShowAssetPreviewAttribute.cs
@@ -5,10 +5,13 @@ namespace NaughtyAttributes
 	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
 	public class ShowAssetPreviewAttribute : DrawerAttribute
 	{
+		public const int DefaultWidth = 64;
+		public const int DefaultHeight = 64;
+
 		public int Width { get; private set; }
 		public int Height { get; private set; }
 
-		public ShowAssetPreviewAttribute(int width = 64, int height = 64)
+		public ShowAssetPreviewAttribute(int width = DefaultWidth, int height = DefaultHeight)
 		{
 			Width = width;
 			Height = height;
