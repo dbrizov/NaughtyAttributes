7a671e34476296c7e99810161113794e782ee61c
dbrizov <d.b.rizov@gmail.com>
2020-11-02 18:40:29 +0200

    CI test

--- Scripts/Editor/NaughtyInspector.cs
+++ Scripts/Editor/NaughtyInspector.cs
@@ -16,6 +16,7 @@ namespace NaughtyAttributes.Editor
 		private IEnumerable<MethodInfo> _methods;
 		private Dictionary<string, SavedBool> _foldouts = new Dictionary<string, SavedBool>();
 
+
 		protected virtual void OnEnable()
 		{
 			_nonSerializedFields = ReflectionUtility.GetAllFields(
