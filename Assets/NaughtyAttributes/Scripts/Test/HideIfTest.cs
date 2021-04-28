fa8c0d27674c24501183dad031c9044f1ca62be2
IF-ACT <xyyjxl@outlook.com>
2021-04-28 14:30:49 +0800

    Create tests

--- Assets/NaughtyAttributes/Scripts/Test/HideIfTest.cs
+++ Assets/NaughtyAttributes/Scripts/Test/HideIfTest.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace NaughtyAttributes.Test
 {
@@ -7,6 +8,7 @@ namespace NaughtyAttributes.Test
 		public bool hide1;
 		public bool hide2;
 		public HideIfEnum enum1;
+		[EnumFlags] public HideIfEnumFlag enum2;
 
 		[HideIf(EConditionOperator.And, "hide1", "hide2")]
 		[ReorderableList]
@@ -20,6 +22,14 @@ namespace NaughtyAttributes.Test
 		[ReorderableList]
 		public int[] hideIfEnum;
 
+		[HideIf("enum2", HideIfEnumFlag.Flag0)]
+		[ReorderableList]
+		public int[] hideIfEnumFlag;
+
+		[HideIf("enum2", HideIfEnumFlag.Flag0 | HideIfEnumFlag.Flag1)]
+		[ReorderableList]
+		public int[] hideIfEnumFlagMulti;
+
 		public HideIfNest1 nest1;
 	}
 
@@ -29,9 +39,11 @@ namespace NaughtyAttributes.Test
 		public bool hide1;
 		public bool hide2;
 		public HideIfEnum enum1;
+		[EnumFlags] public HideIfEnumFlag enum2;
 		public bool Hide1 { get { return hide1; } }
 		public bool Hide2 { get { return hide2; } }
 		public HideIfEnum Enum1 { get { return enum1; } }
+		public HideIfEnumFlag Enum2 { get { return enum2; } }
 
 		[HideIf(EConditionOperator.And, "Hide1", "Hide2")]
 		[AllowNesting] // Because it's nested we need to explicitly allow nesting
@@ -45,6 +57,14 @@ namespace NaughtyAttributes.Test
 		[AllowNesting] // Because it's nested we need to explicitly allow nesting
 		public int hideIfEnum;
 
+		[HideIf("Enum2", HideIfEnumFlag.Flag0)]
+		[AllowNesting]
+		public int hideIfEnumFlag;
+
+		[HideIf("Enum2", HideIfEnumFlag.Flag0 | HideIfEnumFlag.Flag1)]
+		[AllowNesting]
+		public int hideIfEnumFlagMulti;
+
 		public HideIfNest2 nest2;
 	}
 
@@ -54,9 +74,11 @@ namespace NaughtyAttributes.Test
 		public bool hide1;
 		public bool hide2;
 		public HideIfEnum enum1;
+		[EnumFlags] public HideIfEnumFlag enum2;
 		public bool GetHide1() { return hide1; }
 		public bool GetHide2() { return hide2; }
 		public HideIfEnum GetEnum1() { return enum1; }
+		public HideIfEnumFlag GetEnum2() { return enum2; }
 
 		[HideIf(EConditionOperator.And, "GetHide1", "GetHide2")]
 		[MinMaxSlider(0.0f, 1.0f)] // AllowNesting attribute is not needed, because the field is already marked with a custom naughty property drawer
@@ -69,6 +91,14 @@ namespace NaughtyAttributes.Test
 		[HideIf("GetEnum1", HideIfEnum.Case2)]
 		[MinMaxSlider(0.0f, 1.0f)] // AllowNesting attribute is not needed, because the field is already marked with a custom naughty property drawer
 		public Vector2 hideIfEnum = new Vector2(0.25f, 0.75f);
+
+		[HideIf("GetEnum2", HideIfEnumFlag.Flag0)]
+		[MinMaxSlider(0.0f, 1.0f)] // AllowNesting attribute is not needed, because the field is already marked with a custom naughty property drawer
+		public Vector2 hideIfEnumFlag;
+
+		[HideIf("GetEnum2", HideIfEnumFlag.Flag0 | HideIfEnumFlag.Flag1)]
+		[MinMaxSlider(0.0f, 1.0f)] // AllowNesting attribute is not needed, because the field is already marked with a custom naughty property drawer
+		public Vector2 hideIfEnumFlagMulti;
 	}
 
 	public enum HideIfEnum
@@ -76,5 +106,14 @@ namespace NaughtyAttributes.Test
 		Case0,
 		Case1,
 		Case2
+	}
+
+	[Flags]
+	public enum HideIfEnumFlag
+	{
+		Flag0 = 1,
+		Flag1 = 2,
+		Flag2 = 4,
+		Flag3 = 8
 	}
 }
