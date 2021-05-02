e4edd04d78aeb27cb99134b8fc84f521485de6de
dbrizov <d.b.rizov@gmail.com>
2021-05-02 21:24:26 +0300

    #232 - fixed a bug where expandable is not working for a list of scriptable objects

--- Assets/NaughtyAttributes/Scripts/Test/_NaughtyScriptableObject.cs
+++ Assets/NaughtyAttributes/Scripts/Test/_NaughtyScriptableObject.cs
@@ -6,28 +6,7 @@ namespace NaughtyAttributes.Test
 	//[CreateAssetMenu(fileName = "NaughtyScriptableObject", menuName = "NaughtyAttributes/_NaughtyScriptableObject")]
 	public class _NaughtyScriptableObject : ScriptableObject
 	{
-		public int integer;
-
-		[MinMaxSlider(0.0f, 1.0f)]
-		public Vector2 minMaxSlider;
-
-		[Dropdown("GetVectorValues")]
-		public Vector3 vectorValue;
-
-		private DropdownList<Vector3> GetVectorValues()
-		{
-			return new DropdownList<Vector3>()
-			{
-				{ "Right", Vector3.right },
-				{ "Up", Vector3.up },
-				{ "Forward", Vector3.forward }
-			};
-		}
-
-		[Button]
-		private void Log()
-		{
-			Debug.Log(vectorValue);
-		}
+		[Expandable]
+		public List<_TestScriptableObject> list;
 	}
 }
