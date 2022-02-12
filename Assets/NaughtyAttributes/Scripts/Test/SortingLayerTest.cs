b5dab421d91f0bb6204bfb90232240bab0d72068
dbrizov <d.b.rizov@gmail.com>
2022-02-12 16:37:15 +0200

    Refactoring

--- Assets/NaughtyAttributes/Scripts/Test/SortingLayerTest.cs
+++ Assets/NaughtyAttributes/Scripts/Test/SortingLayerTest.cs
@@ -2,45 +2,45 @@
 
 namespace NaughtyAttributes.Test
 {
-	public class SortingLayerTest : MonoBehaviour
-	{
-		[SortingLayer]
-		public int layerNumber0;
+    public class SortingLayerTest : MonoBehaviour
+    {
+        [SortingLayer]
+        public int layerNumber0;
 
-		[SortingLayer]
-		public string layerName0;
+        [SortingLayer]
+        public string layerName0;
 
-		public SortingLayerNest1 nest1;
+        public SortingLayerNest1 nest1;
 
-		[Button]
-		public void DebugLog()
-		{
-			Debug.LogFormat("{0} = {1}", nameof(layerNumber0), layerNumber0);
-			Debug.LogFormat("{0} = {1}", nameof(layerName0), layerName0);
-			Debug.LogFormat("LayerToName({0}) = {1}", layerNumber0, SortingLayer.IDToName(layerNumber0));
-			Debug.LogFormat("NameToLayer({0}) = {1}", layerName0, SortingLayer.NameToID(layerName0));
-		}
-	}
+        [Button]
+        public void DebugLog()
+        {
+            Debug.LogFormat("{0} = {1}", nameof(layerNumber0), layerNumber0);
+            Debug.LogFormat("{0} = {1}", nameof(layerName0), layerName0);
+            Debug.LogFormat("LayerToName({0}) = {1}", layerNumber0, SortingLayer.IDToName(layerNumber0));
+            Debug.LogFormat("NameToLayer({0}) = {1}", layerName0, SortingLayer.NameToID(layerName0));
+        }
+    }
 
-	[System.Serializable]
-	public class SortingLayerNest1
-	{
-		[SortingLayer]
-		public int layerNumber1;
+    [System.Serializable]
+    public class SortingLayerNest1
+    {
+        [SortingLayer]
+        public int layerNumber1;
 
-		[SortingLayer]
-		public string layerName1;
+        [SortingLayer]
+        public string layerName1;
 
-		public SortingLayerNest2 nest2;
-	}
+        public SortingLayerNest2 nest2;
+    }
 
-	[System.Serializable]
-	public struct SortingLayerNest2
-	{
-		[SortingLayer]
-		public int layerNumber2;
+    [System.Serializable]
+    public struct SortingLayerNest2
+    {
+        [SortingLayer]
+        public int layerNumber2;
 
-		[SortingLayer]
-		public string layerName2;
-	}
+        [SortingLayer]
+        public string layerName2;
+    }
 }
