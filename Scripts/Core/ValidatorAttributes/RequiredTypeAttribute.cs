29060b2e0c45996ca4900ae868914c2841994cc1
Musa Demir <demirmusa96@gmail.com>
2022-11-30 01:42:32 +0300

    renaming parameter

--- Scripts/Core/ValidatorAttributes/RequiredTypeAttribute.cs
+++ Scripts/Core/ValidatorAttributes/RequiredTypeAttribute.cs
@@ -5,19 +5,19 @@ namespace NaughtyAttributes
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class RequiredTypeAttribute : ValidatorAttribute
     {
-        public Type[] BaseTypes { get; private set; }
+        public Type[] RequiredTypes { get; private set; }
         
         public bool ShowInfoMessageWhenEmpty { get; private set; }
         
-        public RequiredTypeAttribute(params Type[] baseTypes)
+        public RequiredTypeAttribute(params Type[] requiredTypes)
         {
-            BaseTypes = baseTypes;
+            RequiredTypes = requiredTypes;
             ShowInfoMessageWhenEmpty = true;
         }
         
-        public RequiredTypeAttribute(bool showInfoMessageWhenEmpty, params Type[] baseTypes)
+        public RequiredTypeAttribute(bool showInfoMessageWhenEmpty, params Type[] requiredTypes)
         {
-            BaseTypes = baseTypes;
+            RequiredTypes = requiredTypes;
             ShowInfoMessageWhenEmpty = showInfoMessageWhenEmpty;
         }
     }
