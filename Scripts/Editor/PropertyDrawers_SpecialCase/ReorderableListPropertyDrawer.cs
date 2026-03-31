6910ac3118913c4067555e320b65e89d73cce5e4
nnra6864 <nnra2210@gmail.com>
2026-03-31 22:16:24 +0200

    6.5 Fixes

--- Scripts/Editor/PropertyDrawers_SpecialCase/ReorderableListPropertyDrawer.cs
+++ Scripts/Editor/PropertyDrawers_SpecialCase/ReorderableListPropertyDrawer.cs
@@ -26,7 +26,11 @@ namespace NaughtyAttributes.Editor
 
         private string GetPropertyKeyName(SerializedProperty property)
         {
+#if UNITY_6000_5_OR_NEWER
+            return EntityId.ToULong(property.serializedObject.targetObject.GetEntityId()) + "." + property.name;
+#else
             return property.serializedObject.targetObject.GetInstanceID() + "." + property.name;
+#endif
         }
 
         protected override float GetPropertyHeight_Internal(SerializedProperty property)
